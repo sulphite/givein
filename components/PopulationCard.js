a95bb9864307e4b3c342ebb9e2715064bab48beb
Darren Mothersele <darren@darrenmothersele.com>
2023-04-29 15:43:15 +0100

    update pop card

--- components/PopulationCard.js
+++ components/PopulationCard.js
@@ -1,16 +1,23 @@
-import { Card, Metric, Text } from "@tremor/react";
+import { Card, CategoryBar, Metric, Text } from "@tremor/react";
 import { useEffect, useState } from "react";
 
 export function PopulationCard() {
   const [population, setPopulation] = useState(8_032_291_613);
+  const [danger, setDanger] = useState(72);
 
   useEffect(() => {
     const i = setInterval(() => {
       const percentChange = 0.000000001 * (Math.random() - 0.25);
       setPopulation(population - Math.floor(percentChange * population));
-    }, 2000);
+    }, 1700);
+    const j = setInterval(() => {
+      setDanger(60 + Math.random() * 35);
+    }, 3500);
 
-    return () => clearInterval(i);
+    return () => {
+      clearInterval(i);
+      clearInterval(j);
+    };
   });
 
   return (
@@ -19,6 +26,14 @@ export function PopulationCard() {
       {/* <div className="h-28" /> */}
       <Text>Global Population</Text>
       <Metric>{population.toLocaleString("en")}</Metric>
+
+      <Text className="mt-4">Danger Level</Text>
+      <CategoryBar
+        categoryPercentageValues={[40, 30, 20, 10]}
+        colors={["emerald", "yellow", "orange", "rose"]}
+        percentageValue={danger}
+        className="mt-3"
+      />
     </Card>
   );
 }
