d452bfe1fc4442fe3a8a0c4a9b1524c7e6479506
Darren Mothersele <darren@darrenmothersele.com>
2023-04-29 16:19:29 +0100

    add tracker

--- components/StatCard3.js
+++ components/StatCard3.js
@@ -1,10 +1,42 @@
-import { Card } from "@tremor/react";
+import { Card, Flex, Title, Tracker, Text } from "@tremor/react";
+
+const data = [
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "rose", tooltip: "Downtime" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "gray", tooltip: "Maintenance" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "emerald", tooltip: "Operational" },
+  { color: "yellow", tooltip: "Degraded" },
+  { color: "emerald", tooltip: "Operational" },
+];
 
 export function StatCard3() {
   return (
     <Card>
-      {/* Placeholder to set height */}
-      <div className="h-28" />
+      <Title>Status</Title>
+      <Text>Destroy-bot Apr 2023</Text>
+      <Flex justifyContent="end" className="mt-4">
+        <Text>Uptime 92%</Text>
+      </Flex>
+      <Tracker data={data} className="mt-2" />
     </Card>
   );
 }
