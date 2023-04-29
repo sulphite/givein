1699118f07e68a2ecbcce02c567065ef1992a65f
Darren Mothersele <darren@darrenmothersele.com>
2023-04-29 16:29:30 +0100

    display change in population

--- components/PopulationCard.js
+++ components/PopulationCard.js
@@ -1,14 +1,33 @@
-import { Card, CategoryBar, Metric, Text } from "@tremor/react";
+import {
+  BadgeDelta,
+  Card,
+  CategoryBar,
+  Flex,
+  Metric,
+  Text,
+} from "@tremor/react";
 import { useEffect, useState } from "react";
 
 export function PopulationCard() {
   const [population, setPopulation] = useState(8_032_291_613);
+  const [populationChange, setPopulationChange] = useState(`0`);
   const [danger, setDanger] = useState(72);
+  const [deltaType, setDeltaType] = useState("unchanged");
 
   useEffect(() => {
     const i = setInterval(() => {
       const percentChange = 0.000000001 * (Math.random() - 0.25);
+      setPopulationChange(
+        `${Math.abs(Math.floor(percentChange * population))}`
+      );
       setPopulation(population - Math.floor(percentChange * population));
+      if (Math.floor(percentChange * population) === 0) {
+        setDeltaType("unchanged");
+      } else {
+        setDeltaType(
+          percentChange > 0 ? "moderateDecrease" : "moderateIncrease"
+        );
+      }
     }, 1700);
     const j = setInterval(() => {
       setDanger(60 + Math.random() * 35);
@@ -22,9 +41,11 @@ export function PopulationCard() {
 
   return (
     <Card>
-      {/* Placeholder to set height */}
-      {/* <div className="h-28" /> */}
-      <Text>Global Population</Text>
+      <Flex alignItems="start">
+        <Text>Global Population</Text>
+        <BadgeDelta deltaType={deltaType}>{populationChange}</BadgeDelta>
+      </Flex>
+
       <Metric>{population.toLocaleString("en")}</Metric>
 
       <Text className="mt-4">Danger Level</Text>
