8be5c2e41f5b80d34fdc096beda16ee0c4d5853a
Darren Mothersele <darren@darrenmothersele.com>
2023-04-29 16:15:28 +0100

    add plan

--- components/InfoCard1.js
+++ components/InfoCard1.js
@@ -1,10 +1,25 @@
-import { Card } from "@tremor/react";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
+import { useContext } from "react";
+import MessageContext from "@/contexts/messages";
 
 export function InfoCard1() {
+  const { messages } = useContext(MessageContext);
+
+  let plan = [];
+  if (messages.length > 0 && messages[0].plan) {
+    plan = messages[0].plan.split("\n").map((s) => s.substring(2));
+  }
+
   return (
-    <Card>
-      {/* Placeholder to set height */}
-      <div className="h-64" />
+    <Card className="h-64">
+      <Subtitle>Plan</Subtitle>
+      {plan.length === 0 && <Text>Thinking...</Text>}
+      {plan.map((p, i) => (
+        <div>
+          {i !== 0 && <Divider />}
+          {p}
+        </div>
+      ))}
     </Card>
   );
 }
